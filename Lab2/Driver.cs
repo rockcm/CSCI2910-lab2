c1380a772bef43f0fe6d517622d7c665fba5e693
rockcm <rockcm@etsu.edu>
2023-09-12 18:59:22 -0400

    lab2

---
+++ Lab2/Driver.cs
@@ -0,0 +1,114 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+//	Solution/Project:  Lab 2 
+//	File Name:         VideoGame.cs
+//	Description:       class to create a vidoegame object for use in driver.  
+//	Course:            CSCI-2910
+//	Author:            Christian Rock, rockcm@etsu.edu, East Tennessee State University
+//	Created:           09/12/23
+//	Copyright:         Christian Rock, 2023/
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+using Lab1;
+using System.Xml.Linq;
+///variables 
+List<VideoGame> videoGames = new List<VideoGame>();
+StreamReader streamReader = null;
+Dictionary<string, List<VideoGame>> vgDictionary = new Dictionary<string, List<VideoGame>>();
+string key = null;
+
+
+
+
+
+// try catch for streamreader 
+try
+{
+
+
+    // creating a stream reader to read in the file given. 
+    streamReader = new StreamReader(@"../../../gameList/videogames.csv");
+
+    streamReader.ReadLine(); // reading first line of document
+                             // while stream reader is not at the end of doc, continue loop. 
+    while (streamReader.Peek() != -1)
+    {
+        // splitting each line read in to fiels that can be accessed and used to create videogame item. 
+        string textLine = streamReader.ReadLine();
+        string[] fields = textLine.Split(",");
+        VideoGame Videogame = new VideoGame(fields[0], fields[1], fields[2], fields[3], fields[4], Convert.ToDecimal(fields[5]), Convert.ToDecimal(fields[6]), Convert.ToDecimal(fields[7]), Convert.ToDecimal(fields[8]), Convert.ToDecimal(fields[9]));
+        videoGames.Add(Videogame);
+        
+
+        
+
+        
+
+    }
+
+
+
+}
+catch (FormatException e)
+{
+    Console.WriteLine($"{e.Message}");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+finally // makes sure reader closes regardless
+{
+    if (streamReader != null)
+        streamReader.Close();
+}
+
+//creating a dictionary from list.. grouped by platform 
+Dictionary<string, List<VideoGame>> myDictionary = videoGames
+.GroupBy(o => o.platform).ToDictionary(g => g.Key, g => g.ToList());
+
+//going through each key and taking the top 5 of each platforms global sales 
+var top5ItemsByKey = myDictionary.ToDictionary(
+           kvp => kvp.Key,
+           kvp => kvp.Value.OrderByDescending(item => item.global_Sales).Take(5).ToList()
+       );
+
+
+//displaying each platforms top 5 selling games 
+foreach (var kvp in top5ItemsByKey)
+{
+    Console.WriteLine($"Key: {kvp.Key}");
+    foreach (var item in kvp.Value)
+    {
+        Console.WriteLine($"Name: {item.name}, Sales: {item.global_Sales}");
+    }
+    Console.WriteLine();
+}
+
+
+
+/*
+// prints all global sales for all platforms 
+foreach (var v in myDictionary)
+{
+    Console.WriteLine($"{v.Key}"); // list attatched to each key. 
+    for (int i =0; i < myDictionary[v.Key].Count; i++)
+    {
+       Console.WriteLine(myDictionary[v.Key][i].global_Sales);
+    }
+
+}
+*/
+
+
+
+
+
+
+
+
+
---
+++ Lab2/VideoGame.cs
@@ -0,0 +1,126 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Solution/Project:  Lab 2 
+//	File Name:         VideoGame.cs
+//	Description:       class to create a vidoegame object for use in driver.  
+//	Course:            CSCI-2910
+//	Author:            Christian Rock, rockcm@etsu.edu, East Tennessee State University
+//	Created:           08/29/2023
+//	Copyright:         Christian Rock, 2023
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    /// <summary>
+    /// class to create video game objects that can be compared to each other
+    /// </summary>
+    public class VideoGame : IComparable<VideoGame>
+    {
+        //properties for videogame object
+        public string name { get; set; }
+        public string platform { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public string publisher { get; set; }
+        public decimal na_Sales { get; set; }
+        public decimal eu_Sales { get; set; }
+        public decimal jp_Sales { get; set; }
+        public decimal other_Sales { get; set; }
+        public decimal global_Sales { get; set; }
+
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public VideoGame()
+        {
+            this.name = string.Empty;
+            this.platform = string.Empty;
+            this.year = string.Empty;
+            this.genre = string.Empty;
+            this.publisher = string.Empty;
+            this.na_Sales = 0;
+            this.eu_Sales = 0;
+            this.jp_Sales = 0;
+            this.other_Sales = 0;
+            this.global_Sales = 0;
+        }
+
+        /// <summary>
+        /// parameterized constructor
+        /// </summary>
+        /// <param name="name">name of game</param>
+        /// <param name="platform">platform game is on</param>
+        /// <param name="year">year game is released</param>
+        /// <param name="genre">genre of game</param>
+        /// <param name="publisher">publisher of game</param>
+        /// <param name="na_Sales">na sales for game</param>
+        /// <param name="eu_Sales">eu sales for game</param>
+        /// <param name="jp_Sales">jp sales for game</param>
+        /// <param name="other_Sales">other sales for game</param>
+        /// <param name="global_Sales">global sales for game</param>
+        public VideoGame(string name, string platform, string year, string genre, string publisher, decimal na_Sales, decimal eu_Sales, decimal jp_Sales, decimal other_Sales, decimal global_Sales)
+        {
+            this.name = name;
+            this.platform = platform;
+            this.year = year;
+            this.genre = genre;
+            this.publisher = publisher;
+            this.na_Sales = na_Sales;
+            this.eu_Sales = eu_Sales;
+            this.jp_Sales = jp_Sales;
+            this.other_Sales = other_Sales;
+            this.global_Sales = global_Sales;
+        }
+        /// <summary>
+        /// copy constructor 
+        /// </summary>
+        /// <param name="videoGame"> video game object</param>
+        public VideoGame(VideoGame videoGame)
+        {
+            this.name = videoGame.name;
+            this.platform = videoGame.platform;
+            this.year = videoGame.year;
+            this.genre = videoGame.genre;
+            this.publisher = videoGame.publisher;
+            this.na_Sales = videoGame.na_Sales;
+            this.eu_Sales = videoGame.eu_Sales;
+            this.jp_Sales = videoGame.jp_Sales;
+            this.other_Sales = videoGame.other_Sales;
+            this.global_Sales = videoGame.global_Sales;
+        }
+        /// <summary>
+        /// comapares the name of one videogame object to another
+        /// </summary>
+        /// <param name="obj">video game object</param>
+        /// <returns></returns>
+        public int CompareTo(VideoGame obj) => string.Compare(name, obj.name);
+
+
+        /// <summary>
+        /// to string that displays all the info for a game appropriately 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string str = $"Name: {name}\n";
+            str += $"Platform: {platform}\n";
+            str += $"Genre: {genre}\n";
+            str += $"Publisher: {publisher}\n";
+            str += $"NA Sales: {na_Sales}\n";
+            str += $"EU Sales: {eu_Sales}\n";
+            str += $"JP Sales: {jp_Sales}\n";
+            str += $"Other Sales: {other_Sales}\n";
+            return str;
+        }// end ToString
+
+
+    }
+}
